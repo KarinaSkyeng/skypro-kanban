40f0becc51b66385cb27c083c1e6a6264f30d90a
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-06-02 17:32:52 +0200

    убрала ошибку из консоли

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Column } from "./components/Column";
-import { Card } from "./components/Card";
 import { PopBrowse } from "./components/PopBrowse";
 import { PopNewCard } from "./components/PopNewCard";
 import { PopUser } from "./components/PopUser";
-import { Calendar } from "./components/Calendar";
 
 import './App.css'
 
@@ -13,14 +11,12 @@ function App() {
 
   return (
     <div className="wrapper">
-	<Header />		
-  <Main />
-  <Column />
-  <Card />
-  <PopBrowse />
-  <PopNewCard />
-  <PopUser />
-  <Calendar />
+        <Header />		
+        <Main />
+        <Column />       
+        <PopBrowse />
+        <PopNewCard />
+        <PopUser />
     </div>
     );
 }
