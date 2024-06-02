3ffd0ecfa682342aa526d564489b6fb25596709a
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-06-02 11:15:42 +0200

    Доработка дз

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { Column } from "./components/Column";
+import { Card } from "./components/Card";
+import { PopBrowse } from "./components/PopBrowse";
+import { PopNewCard } from "./components/PopNewCard";
+import { PopUser } from "./components/PopUser";
+import { Calendar } from "./components/Calendar";
+
 import './App.css'
 
 function App() {  
@@ -8,6 +15,12 @@ function App() {
     <div className="wrapper">
 	<Header />		
   <Main />
+  <Column />
+  <Card />
+  <PopBrowse />
+  <PopNewCard />
+  <PopUser />
+  <Calendar />
     </div>
     );
 }
