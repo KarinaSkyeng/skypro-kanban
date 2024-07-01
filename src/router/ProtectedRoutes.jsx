3cd21888eba34e52a2affc982dacff4cb704b007
Karina Rozenberga Korneva <karinilya@mail.ru>
2024-07-01 20:36:26 +0200

    Исправила логин

--- src/router/ProtectedRoutes.jsx
+++ src/router/ProtectedRoutes.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom"
-
 import { routes } from "./routes";
+import { useUserContext } from "../context/useUserContext";
 
-export const ProtectedRoutes = ({user}) => {
-    
+export const ProtectedRoutes = () => {
+    const {user} = useUserContext()
     return user ? <Outlet /> : <Navigate to={routes.login} />
 }
